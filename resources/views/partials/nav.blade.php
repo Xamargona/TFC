e1c611df2c5f7bb9da35970b044fa317200abc36
xamargona <jamargon14@gmail.com>
2023-05-26 06:26:21 +0200

    zzz

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -9,7 +9,7 @@
         @if (Auth::check())
             <div class="flex items-center md:order-2">
                 <button type="button"
-                    class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                    class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 "
                     id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
                     data-dropdown-placement="bottom">
                     <span class="sr-only">Open user menu</span>
@@ -23,51 +23,51 @@
                     @endif
                 </button>
                 <!-- Dropdown menu -->
-                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow "
                     id="user-dropdown">
                     <div class="px-4 py-3">
                         <span class="block text-sm text-gray-900 ">{{ Auth::user()->username }}</span>
                         <span
-                            class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ Auth::user()->email }}</span>
+                            class="block text-sm  text-gray-500 truncate ">{{ Auth::user()->email }}</span>
                     </div>
                     <ul class="py-2" aria-labelledby="user-menu-button">
                         <li>
                             <a href="{{ route('users.show', Auth::user()->id) }}"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Ver
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ">Ver
                                 perfil</a>
                         </li>
                         <li>
                             <a href="{{ route('users.edit', Auth::user()->id) }}"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Editar
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ">Editar
                                 perfil</a>
                         </li>
                         @if (Auth::user()->role == 'artist')
                             <li>
                                 <a href="{{ route('publications.create') }}"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Publicar</a>
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ">Publicar</a>
                             </li>
                             <li>
                                 <a href="{{ route('bookings.index') }}"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Reservas</a>
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ">Reservas</a>
                             </li>
                         @endif
                         @if (Auth::user()->role == 'admin')
                             <li>
                                 <a href="{{ route('events.create') }}"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Crear evento</a>
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ">Crear evento</a>
                             </li>
                             <li>
                                 <a href="{{ route('contactMessages.index') }}"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Ver incidencias</a>
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ">Ver incidencias</a>
                             </li>
                         @endif
                         <li>
                             <a href="{{ route('chats.index') }}"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Ver chats</a>
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ">Ver chats</a>
                         </li>
                         <li>
                             <a href="{{ route('logout') }}"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ">Sign
                                 out</a>
                         </li>
                     </ul>
